3d966174d62d0372fae857f223b5ca0e41d45a6d
njogukharim <dankhareem@gmail.com>
2019-10-22 17:32:22 +0300

    undefined or null

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -51,7 +51,7 @@ describe("basics", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+        const someVariable=undefined;
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof someVariable).to.equal("undefined");
